39503b9318b7ce3d2219caeceb56aab23b5526ee
atarmanipandey <nitw.atar@gmail.com>
2015-12-30 15:34:51 +0530

    initial

---
+++ src/main/java/psv/apps/java/retrofitexample/GetWeatherRestAdapter.java
@@ -0,0 +1,40 @@
+package psv.apps.java.retrofitexample;
+
+import android.util.Log;
+
+import retrofit.Callback;
+import retrofit.RestAdapter;
+
+/**
+ * Created by ajaythakur on 6/16/15.
+ */
+public class GetWeatherRestAdapter {
+    protected final String TAG = "atar";
+    protected RestAdapter mRestAdapter;
+    protected GetWeatherApi mApi;
+    static final String WEATHER_URL="http://api.openweathermap.org";
+   // static final String OPEN_WEATHER_API = "51337ba29f38cb7a5664cda04d84f4cd";
+    static final String OPEN_WEATHER_API = "55b746c1f948accb79972afcbc3da239";
+
+    public GetWeatherRestAdapter() {
+        mRestAdapter = new RestAdapter.Builder()
+                .setLogLevel(RestAdapter.LogLevel.FULL)
+                .setEndpoint(WEATHER_URL)
+                .setErrorHandler(new WeatherApiErrorHandler())
+                .build();
+        mApi = mRestAdapter.create(GetWeatherApi.class); // create the interface
+        Log.d(TAG, "GetWeatherRestAdapter -- created");
+    }
+
+    public void testWeatherApi(String city, Callback<WeatherData> callback){
+        Log.d(TAG, "testWeatherApi: for city:" + city);
+        mApi.getWeatherFromApi(city, OPEN_WEATHER_API, callback);
+    }
+
+    public WeatherData testWeatherApiSync(String city) {
+        WeatherData result;
+        Log.d(TAG, "testWeatherApi: for city:" + city);
+        result = mApi.getWeatherFromApiSync(city,OPEN_WEATHER_API);
+        return result;
+    }
+}
